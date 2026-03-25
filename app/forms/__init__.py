37818fc3bfa861cff7acf4413ab4dba487fb0786
silviolpires <silviolpires.dev@gmail.com>
2026-03-25 15:36:43 -0300

    primeiro commit

---
+++ app/forms/__init__.py
@@ -0,0 +1,1 @@
+from .cliente_forms import ClienteForm, EnderecoForm
---
+++ app/views/dependente_views.py
@@ -0,0 +1,15 @@
+from django.views.generic import CreateView, ListView
+from app.forms.dependente_forms import DependenteForm
+from app.models import Dependente
+
+
+class DependenteCreatView(CreateView):
+    model = Dependente
+    form_class = DependenteForm
+    template_name = "dependentes/form_dependente.html"
+    success_url = "lista_dependente"
+    
+    
+class DependenteListView(ListView):
+    model = Dependente
+    template_name = "dependentes/lista_dependentes.html"
